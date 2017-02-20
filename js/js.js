a1d03a2bf6c534a8ffec99740cedd7f94cfe6731
Jandro <jandro.suarez.92@gmail.com>
2017-02-20 10:17:48 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -3,73 +3,119 @@ var v = 0;
 var g = 1.622;
 var a = g;
 var dt = 0.016683;
-var timer=null;
-var timerFuel=null;
-var fuel=100;
+var timer = null;
+var timerFuel = null;
+var fuel = 100;
+var juegoActivo = true;
 
 //al cargar por completo la página...
-window.onload = function(){
-	//definición de eventos
-	//mostrar menú móvil
-    	document.getElementById("showm").onclick = function () {
-		document.getElementsByClassName("c")[0].style.display = "block";
-		stop();
-	}
-	//ocultar menú móvil
-	document.getElementById("hidem").onclick = function () {
-		document.getElementsByClassName("c")[0].style.display = "none";
-		start();
-	}
-	//encender/apagar el motor al hacer click en la pantalla
-	document.onclick = function () {
- 	  if (a==g){
-  		motorOn();
- 	  } else {
-  		motorOff();
- 	  }
-	}
-	//encender/apagar al apretar/soltar una tecla
-	document.onkeydown = motorOn;
-	document.onkeyup = motorOff;
+window.onload = function() {
+    //definición de eventos
+    //mostrar menú móvil
+    //  	document.getElementById("showm").onclick = function () {
+    //document.getElementsByClassName("c")[0].style.display = "block";
+    //stop();
+    //}
+    //ocultar menú móvil
+    //document.getElementById("hidem").onclick = function () {
+    //	document.getElementsByClassName("c")[0].style.display = "none";
+    //	start();
+    //}
+    //encender/apagar el motor al hacer click en la pantalla
+    document.onclick = function() {
+        if (a == g) {
+            motorOn();
+        } else {
+            motorOff();
+        }
+    }
+    //encender/apagar al apretar/soltar una tecla
+    document.onkeydown = motorOn;
+    document.onkeyup = motorOff;
 
-	//Empezar a mover nave
-	start();
+    //Empezar a mover nave
+    start();
 }
 
 //Definición de funciones
-function start(){
-	timer=setInterval(function(){ moverNave(); }, dt*1000);
+function start() {
+    timer = setInterval(function() {
+        moverNave();
+    }, dt * 1000);
 }
 
-function stop(){
-	clearInterval(timer);
+function stop() {
+    clearInterval(timer);
 }
 
-function moverNave(){
-	v +=a*dt;
-	document.getElementById("speed").innerHTML=v;
-	y +=v*dt;
-	document.getElementById("altitude").innerHTML=y;
+function moverNave() {
+    v += a * dt;
+    document.getElementById("speed").innerHTML = v;
+    y += v * dt;
+    document.getElementById("altitude").innerHTML = y;
 
-	//mover hasta que top sea un 70% de la pantalla
-	if (y<70){
-		document.getElementById("nave").style.top = y+"%";
-	} else {
-		stop();
-	}
+    //mover hasta que top sea un 70% de la pantalla
+    if (y < 70) {
+        document.getElementById("nave").style.top = y + "%";
+    } else {
+        juegoActivo = false;
+        stop();
+
+        if (v > 5) {
+            document.getElementById("cohete").src = "../img/explosion.gif"
+        }
+    }
+
 }
-function motorOn(){
-	a=-g;
-	if (timerFuel==null)
-	timerFuel=setInterval(function(){ actualizarFuel(); }, 10);
+
+function motorOn() {
+    if (juegoActivo == true) {
+        a = -g;
+        if (timerFuel == null) {
+            document.getElementById("cohete").src = "../img/nave-fuego.png"
+            timerFuel = setInterval(function() {
+                actualizarFuel();
+            }, 10);
+        }
+        if (fuel < 0) {
+            motorOff();
+        }
+    }
 }
-function motorOff(){
-	a=g;
-	clearInterval(timerFuel);
-	timerFuel=null;
+
+function motorOff() {
+    if (juegoActivo == true) {
+        a = g;
+        clearInterval(timerFuel);
+        timerFuel = null;
+        document.getElementById("cohete").src = "../img/nave.png"
+    }
 }
-function actualizarFuel(){
-	//Aquí hay que cambiar el valor del marcador de Fuel...
-	fuel-=0.1;
-	document.getElementById("fuel").innerHTML=fuel;
+
+function actualizarFuel() {
+    if (juegoActivo == true)
+        fuel -= 0.1;
+    document.getElementById("fuel").innerHTML = fuel;
+}
+
+function reset() {
+    location.reload(true);
+}
+
+function stopGame() {
+    if (juegoActivo == true) {
+        motorOff();
+        stop();
+        juegoActivo = false;
+        document.getElementById("pausa").style.display = "none";
+        document.getElementById("continuar").style.display = "inline-block"
+    }
+}
+
+function continueGame() {
+    juegoActivo = true;
+    start();
+    document.getElementById("continuar").style.display = "none";
+    document.getElementById("pausa").style.display = "inline-block";
+
 }
